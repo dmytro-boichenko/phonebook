0aa522ac18c5be8a962de524da3c5d6d4f295236
Dmytro Boichenko <boichenko.dmitriy@gmail.com>
2018-08-19 00:28:27 +0200

    Configuration for data cascade deletion

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS phonebook;
 
-CREATE TABLE IF NOT EXISTS phonebook.User (
+CREATE TABLE IF NOT EXISTS phonebook.user (
   user_id INTEGER(10) NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
@@ -8,17 +8,23 @@ CREATE TABLE IF NOT EXISTS phonebook.User (
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS phonebook.Contact (
+CREATE TABLE IF NOT EXISTS phonebook.contact (
   contact_id INTEGER (10) NOT NULL AUTO_INCREMENT,
   user_id INTEGER(10) NOT NULL,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
-  PRIMARY KEY (contact_id)
+  PRIMARY KEY (contact_id),
+  FOREIGN KEY (user_id)
+    REFERENCES phonebook.user (user_id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS phonebook.Phone (
+CREATE TABLE IF NOT EXISTS phonebook.phone (
   phone_id INTEGER (10) NOT NULL AUTO_INCREMENT,
   contact_id INTEGER(10) NOT NULL,
   phone VARCHAR(40) NOT NULL,
-  PRIMARY KEY (phone_id)
+  PRIMARY KEY (phone_id),
+  FOREIGN KEY (contact_id)
+    REFERENCES phonebook.contact (contact_id)
+    ON DELETE CASCADE
 );
