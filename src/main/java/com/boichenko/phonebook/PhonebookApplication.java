baf2ed86dfebbb5ed6f25457433ab1fce1878eee
Dmytro Boichenko <boichenko.dmitriy@gmail.com>
2018-08-18 16:42:41 +0200

    Initial commit

---
+++ src/main/java/com/boichenko/phonebook/PhonebookApplication.java
@@ -0,0 +1,12 @@
+package com.boichenko.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookApplication.class, args);
+	}
+}
